fa092f1bdd4794853df9025dd824db68d4bd3a35
Cesar Ivan Rivera <cesarivan25479@gmail.com>
2022-05-09 18:35:52 -0500

    Pasar Busqueda cliente por get

--- js/clientes.js
+++ js/clientes.js
@@ -1,6 +1,6 @@
 document.getElementById("buscarCliente").addEventListener('keydown', ()=>{
     let tecla = event.keyCode;
-    tecla == 40 ? $('#tablaClientes').load('../pages/tablas/tablaClientes.php') : '' ;
+    tecla == 40 ? $('#tablaClientes').load('../pages/tablas/tablaClientes.php?nombre=2022&') : '' ;
 }); 
 document.getElementById("btnBuscarCliente").addEventListener('click',() =>{
     alert("Boton");
