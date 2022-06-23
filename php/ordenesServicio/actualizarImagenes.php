f43b83d44cbec57a61d9ceffc2dc1368ac5e49a3
Cesar Ivan Rivera <cesarivan25479@gmail.com>
2022-06-23 10:03:04 -0500

    Correcion al actulizar imagen

--- php/ordenesServicio/actualizarImagenes.php
+++ php/ordenesServicio/actualizarImagenes.php
@@ -1,4 +1,5 @@
 <?php
+
 function validarArchivo($tipo, $tamano, $nombre)
 {
     $tipoArchvio = array("image/jpeg", "image/jpg", "image/png", "image/gif");
@@ -12,43 +13,45 @@ function validarArchivo($tipo, $tamano, $nombre)
         return "tipoarchvio";
     }
 }
-function guardaImagen($nombre){
+function guardaImagen($nombre)
+{
     include "../../php/ConexionMySQL.php";
     $folio = $_POST["folioAct"];
-
     if ($nombre == "orden") {
         $datosImg = $_FILES['imgOrden']['tmp_name'] ?? "";
         $imgExt = $folio . "orden." . pathinfo($_FILES['imgOrden']['name'], PATHINFO_EXTENSION);
         $destino = $_SERVER['DOCUMENT_ROOT'] . '/Emenet/imagenesOrden/orden/';
+
+        $query2 = "SELECT ImgOrden FROM ordenes WHERE Folio = '$folio'";
+        $result2 = mysqli_query($Conexion, $query2);
+        $borrarImagen = mysqli_fetch_array($result2);
+        unlink($destino . $borrarImagen["ImgOrden"]);
     } else if ($nombre == "credencial") {
         $datosImg = $_FILES['imgCredencial']['tmp_name'] ?? "";
         $imgExt = $folio . "credencial." . pathinfo($_FILES['imgCredencial']['name'], PATHINFO_EXTENSION) ?? "";
         $destino = $_SERVER['DOCUMENT_ROOT'] . '/Emenet/imagenesOrden/credencial/';
+
+        $query2 = "SELECT ImgCredencial FROM ordenes WHERE Folio = '$folio'";
+        $result2 = mysqli_query($Conexion, $query2);
+        $borrarImagen = mysqli_fetch_array($result2);
+        unlink($destino . $borrarImagen["ImgCredencial"]);
     } else if ($nombre == "compromiso") {
         $datosImg = $_FILES['imgComp']['tmp_name'] ?? "";
         $imgExt = $folio . "compromiso." . pathinfo($_FILES['imgComp']['name'], PATHINFO_EXTENSION) ?? "";
         $destino = $_SERVER['DOCUMENT_ROOT'] . '/Emenet/imagenesOrden/compromiso/';
+
+        $query2 = "SELECT ImgCompromiso FROM ordenes WHERE Folio = '$folio'";
+        $result2 = mysqli_query($Conexion, $query2);
+        $borrarImagen = mysqli_fetch_array($result2);
+        unlink($destino . $borrarImagen["ImgCompromiso"]);
     }
 
     if (move_uploaded_file($datosImg, $destino . $imgExt)) {
         if ($nombre == "orden") {
-            $query2 = "SELECT ImgOrden FROM ordenes WHERE Folio = '$folio'";
-            $result2 = mysqli_query($Conexion, $query2);
-            $borrarImagen = mysqli_fetch_array($result2);
-            unlink($destino.$borrarImagen["ImgOrden"]);
             $query = "UPDATE ordenes SET ImgOrden = '$imgExt' WHERE Folio = '$folio'";
-            
         } else if ($nombre == "credencial") {
-            $query2 = "SELECT ImgCredencial FROM ordenes WHERE Folio = '$folio'";
-            $result2 = mysqli_query($Conexion, $query2);
-            $borrarImagen = mysqli_fetch_array($result2);
-            unlink($destino.$borrarImagen["ImgCredencial"]);
             $query = "UPDATE ordenes SET ImgCredencial = '$imgExt' WHERE Folio = '$folio'";
         } else if ($nombre == "compromiso") {
-            $query2 = "SELECT ImgCompromiso FROM ordenes WHERE Folio = '$folio'";
-            $result2 = mysqli_query($Conexion, $query2);
-            $borrarImagen = mysqli_fetch_array($result2);
-            unlink($destino.$borrarImagen["ImgCompromiso"]);
             $query = "UPDATE ordenes SET ImgCompromiso = '$imgExt' WHERE Folio = '$folio'";
         }
         $result = mysqli_query($Conexion, $query);
@@ -57,7 +60,7 @@ function guardaImagen($nombre){
         } else {
             return "sinconexion";
         }
-    }else{
+    } else {
         return "error";
     }
 }
