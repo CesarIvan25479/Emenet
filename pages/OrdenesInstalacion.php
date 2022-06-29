22d3a201e38b443c297d5d99b26cd538c25974cb
Cesar Ivan Rivera <cesarivan25479@gmail.com>
2022-06-29 09:41:24 -0500

    Cambio usuario

--- pages/OrdenesInstalacion.php
+++ pages/OrdenesInstalacion.php
@@ -87,7 +87,7 @@ $textfin = date("Y-m-t");
             <img src="../dist/img/profile-user.png" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Isaac fuentes Nalgon</a>
+            <a href="#" class="d-block">Usuario Emenet</a>
           </div>
         </div>
 
--- pages/ReportePagos.php
+++ pages/ReportePagos.php
@@ -94,7 +94,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="../dist/img/profile-user.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Isaac fuentes Nalgon</a>
+          <a href="#" class="d-block">Usuario Emenet</a>
         </div>
       </div>
 
